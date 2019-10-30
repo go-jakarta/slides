f9390ccd9f449a5a03040ec13933f77b75199132
Kenneth Shaw <kenshaw@gmail.com>
2019-10-31 06:32:47 +0700

    Adding second gunk presentation

---
+++ 28-gunk/go.mod
@@ -0,0 +1,15 @@
+module gophers.id/slides/28-gunk
+
+require (
+	github.com/golang/lint v0.0.0-20180702182130-06c8688daad7 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/gunk/opt v0.0.0-20190118082020-1439f71a8644 // indirect
+	github.com/kisielk/gotool v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271 // indirect
+	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20191028173616-919d9bdd9fe6
+	google.golang.org/grpc v1.24.0
+)
+
+go 1.13
---
+++ 28-gunk/main.go
@@ -0,0 +1,46 @@
+package main
+
+//go:generate gunk generate ./...
+
+import (
+	"context"
+	"log"
+	"net"
+
+	goog "github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+
+	pb "gophers.id/slides/28-gunk/v1/util"
+)
+
+// Server is a util server.
+type Server struct {
+}
+
+// CheckStatus returns the status of the util server.
+func (s *Server) CheckStatus(ctxt context.Context, req *goog.Empty) (*pb.CheckStatusResponse, error) {
+	return &pb.CheckStatusResponse{
+		Status: pb.Status_OK,
+	}, nil
+}
+
+// Echo returns the passed message.
+func (s *Server) Echo(ctxt context.Context, msg *pb.Message) (*pb.Message, error) {
+	return msg, nil
+}
+
+func (s *Server) UpdateStatus(ctxt context.Context, req *pb.UpdateStatusRequest) (*pb.UpdateStatusResponse, error) {
+	return &pb.UpdateStatusResponse{}, nil
+}
+
+// ENDSERVER OMIT
+
+func main() {
+	l, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUtilServer(s, &Server{})
+	log.Fatal(s.Serve(l))
+}
