bf39e5a6515fcb728ced6b2d39dd036e2b46e6c3
Kenneth Shaw <kenshaw@gmail.com>
2020-02-12 09:24:32 +0700

    oops

--- 29-search/cmd/search/main.go
+++ 29-search/cmd/search/main.go
@@ -9,11 +9,14 @@ import (
 func main() {
 	flag.Parse()
 	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v", err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
+// run is the main application entry point.
 func run() error {
-	return nil
+}
+
+type Server struct {
 }
